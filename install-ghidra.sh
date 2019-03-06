1f161ffaa75b9a60e47d39d868d2f3b6a76bdbe2
Bjoern Kerler <info@revskills.de>
2019-03-06 12:33:10 +0100

    Fix installer script

--- install-ghidra.sh
+++ install-ghidra.sh
@@ -4,10 +4,10 @@ wget https://ghidra-sre.org/ghidra_9.0_PUBLIC_20190228.zip
 unzip ghidra_9.0_PUBLIC_20190228.zip
 ./install-jdk.sh
 ./install-scaling.sh
-cp ghidraRun ghidra-9.0/
-cp run_scaled ghidra-9.0/
-mv ghidra-9.0 ~/ghidra-9.0
+cp ghidraRun ghidra_9.0/
+cp run_scaled ghidra_9.0/
+mv ghidra_9.0 ~/ghidra_9.0
 rm ghidra_9.0_PUBLIC_20190228.zip
-echo "Successfully installed Ghidra 9.0 to ~/ghirda-9.0."
-echo "cd to ~/ghidra-9.0 and run as: ./run_scaled --scaled=1.4 ./ghirdaRun"
+echo "Successfully installed Ghidra 9.0 to ~/ghirda_9.0."
+echo "cd to ~/ghidra_9.0 and run as: ./run_scaled --scaled=1.4 ./ghirdaRun"
 
