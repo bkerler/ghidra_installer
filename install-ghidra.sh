69bffa94a1c8e0e5f4e0f73e91b67d9f43050dfc
Bjoern Kerler <info@revskills.de>
2019-03-06 13:19:03 +0100

    Simplify install

--- install-ghidra.sh
+++ install-ghidra.sh
@@ -4,10 +4,11 @@ wget https://ghidra-sre.org/ghidra_9.0_PUBLIC_20190228.zip
 unzip ghidra_9.0_PUBLIC_20190228.zip
 ./install-jdk.sh
 ./install-scaling.sh
-cp ghidraRun ghidra_9.0/
+cp ghidra ghidra_9.0/
 cp run_scaled ghidra_9.0/
 mv ghidra_9.0 ~/ghidra_9.0
 rm ghidra_9.0_PUBLIC_20190228.zip
 echo "Successfully installed Ghidra 9.0 to ~/ghirda_9.0."
-echo "cd to ~/ghidra_9.0 and run as: ./run_scaled --scale=1.4 ./ghidraRun"
+echo "cd to ~/ghidra_9.0 and run: ./ghidra."
+echo "Edit ghidra for other scaling factors than 1.4."
 
