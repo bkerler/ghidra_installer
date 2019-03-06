6cdc34d5bfd0a0ec484ad77fedc90c86ab249482
Bjoern Kerler <info@revskills.de>
2019-03-06 12:24:55 +0100

    Install to home dir

--- install-ghidra.sh
+++ install-ghidra.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 echo "Downloading ghidra"
-wget https://ghidra-sre.org/ghidra_9.0_PUBLIC_20190228.zip -o /tmp/ghidra9.zip
+wget https://ghidra-sre.org/ghidra_9.0_PUBLIC_20190228.zip
 unzip /tmp/ghidra9.zip
 ./install-jdk.sh
 ./install-scaling.sh
 cp ghidraRun ghidra-9.0/
 cp run_scaled ghidra-9.0/
 mv ghidra-9.0 ~/ghidra-9.0
+rm ghidra_9.0_PUBLIC_20190228.zip
 echo "Successfully installed Ghidra 9.0 to ~/ghirda-9.0."
 echo "cd to ~/ghidra-9.0 and run as: ./run_scaled --scaled=1.4 ./ghirdaRun"
 
