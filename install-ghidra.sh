4eb04665eb4903b5285e0f3fd5e400b3a37eec72
Bjoern Kerler <info@revskills.de>
2019-03-06 13:51:39 +0100

    Add installer icon

--- install-ghidra.sh
+++ install-ghidra.sh
@@ -6,9 +6,13 @@ unzip ghidra_9.0_PUBLIC_20190228.zip
 ./install-scaling.sh
 cp ghidra ghidra_9.0/
 cp run_scaled ghidra_9.0/
-mv ghidra_9.0 ~/ghidra_9.0
+sudo rm -rf /opt/ghidra_9.0
+sudo mv ghidra_9.0 /opt/ghidra_9.0
+cp ghidra.desktop ~/Desktop/ghidra.desktop
+cp ghidra.desktop ~/Schreibtisch/ghidra.desktop
+sudo ln -s /opt/ghidra_9.0/ghidra /usr/bin/ghidra
 rm ghidra_9.0_PUBLIC_20190228.zip
-echo "Successfully installed Ghidra 9.0 to ~/ghirda_9.0."
-echo "cd to ~/ghidra_9.0 and run: ./ghidra."
-echo "Edit ghidra for other scaling factors than 1.4."
+echo "Successfully installed Ghidra 9.0 to /opt/ghirda_9.0."
+echo "Run using: ghidra"
+echo "Edit /opt/ghirda_9.0/ghidra for other scaling factors than 1.4."
 
