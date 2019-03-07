189592362654e5ff916af1e17f8701d27da17aa2
Bjoern Kerler <info@revskills.de>
2019-03-07 08:30:01 +0100

    apt confirm

--- install-scaling.sh
+++ install-scaling.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-sudo apt install xvfb
-sudo apt install xpra
+sudo apt install xvfb xpra -y
 #wget https://raw.githubusercontent.com/kaueraal/run_scaled/master/run_scaled
 #chmod +x ./run_scaled
