be1cfa9e97be4b7a092e42db5845fead04fe3864
Bjoern Kerler <info@revskills.de>
2019-03-26 19:13:38 +0100

    Add ghidra 9.0.1

--- install-ghidra.sh
+++ install-ghidra.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 echo "Downloading ghidra"
-wget https://ghidra-sre.org/ghidra_9.0_PUBLIC_20190228.zip
-unzip ghidra_9.0_PUBLIC_20190228.zip
+wget https://ghidra-sre.org/ghidra_9.0.1_PUBLIC_20190325.zip
+unzip ghidra_9.0.1_PUBLIC_20190325.zip
 ./install-jdk.sh
 ./install-scaling.sh
-cp ghidra ghidra_9.0/
-cp ghidra4K ghidra_9.0/
-cp run_scaled ghidra_9.0/
-sudo rm -rf /opt/ghidra_9.0
-sudo mv ghidra_9.0 /opt/ghidra_9.0
+cp ghidra ghidra/
+cp ghidra4K ghidra/
+cp run_scaled ghidra/
+sudo rm -rf /opt/ghidra
+sudo mv ghidra /opt/ghidra
 cp ghidra.desktop ~/Desktop/ghidra.desktop
 cp ghidra.desktop ~/Schreibtisch/ghidra.desktop
-sudo ln -s /opt/ghidra_9.0/ghidra /usr/bin/ghidra
-sudo ln -s /opt/ghidra_9.0/ghidra4K /usr/bin/ghidra4K
-patch -p0 -i patch.diff /opt/ghidra_9.0/support/launch.sh
-rm ghidra_9.0_PUBLIC_20190228.zip
-echo "Successfully installed Ghidra 9.0 to /opt/ghidra_9.0."
+sudo ln -s /opt/ghidra/ghidra /usr/bin/ghidra
+sudo ln -s /opt/ghidra/ghidra4K /usr/bin/ghidra4K
+patch -p0 -i patch.diff /opt/ghidra/support/launch.sh
+rm ghidra_9.0.1_PUBLIC_20190325.zip
+echo "Successfully installed Ghidra 9.0.1 to /opt/ghidra."
 echo "Run using: ghidra or ghidra4K"
-echo "Edit /opt/ghidra_9.0/ghidra4K for other scaling factors than 1.4."
+echo "Edit /opt/ghidra/ghidra4K for other scaling factors than 1.4."
 
